c645739a0095cf99408bc9c28a32c1f41f88cbf4
yaegassy <yaegassy@users.noreply.github.com>
2021-08-18 13:22:44 +0900

    refactor: "volar.action.splitEditors" commands (also add deps)

--- src/features/splitEditors.ts
+++ src/features/splitEditors.ts
@@ -1,153 +1,144 @@
-import { commands, ExtensionContext, Position, workspace, Document } from 'coc.nvim';
+import { commands, ExtensionContext, Position, workspace } from 'coc.nvim';
+
+import { parse, SFCBlock } from '@vue/compiler-sfc';
+import { ref, computed } from '@vue/reactivity';
+import * as shared from '@volar/shared';
+
+interface SimpleSFCBlock {
+  type: string;
+  start: number;
+  end: number;
+}
 
 export function activate(context: ExtensionContext) {
   context.subscriptions.push(commands.registerCommand('volar.action.splitEditors', onSplit));
 
   async function onSplit() {
+    const getDocDescriptor = useDocDescriptor();
+
     const doc = await workspace.document;
     if (!doc) {
       return;
     }
 
-    let countSFCBlock = 0;
-    const scriptTagRange = getTagRange(doc, 'script');
-    if (scriptTagRange) countSFCBlock++;
-    const templateTagRange = getTagRange(doc, 'template');
-    if (templateTagRange) countSFCBlock++;
-    const styleTagRange = getTagRange(doc, 'style');
-    if (styleTagRange) countSFCBlock++;
+    const descriptor = getDocDescriptor(doc.textDocument.getText());
+    const blocksSet: SFCBlock[][] = [];
 
-    //
-    // MEMO: Full SFC
-    //
-    if (scriptTagRange && templateTagRange && styleTagRange && countSFCBlock === 3) {
-      /** 1 */
-      await workspace.jumpTo(doc.textDocument.uri, Position.create(scriptTagRange.start, scriptTagRange.end));
-      // folding
-      await workspace.nvim.command(`${templateTagRange.start + 1},${templateTagRange.end + 1}fold`);
-      await workspace.nvim.command(`${styleTagRange.start + 1},${styleTagRange.end + 1}fold`);
+    const scriptBlocks: SimpleSFCBlock[] = [];
+    const templateBlocks: SimpleSFCBlock[] = [];
+    const styleBlocks: SimpleSFCBlock[] = [];
+    const customBlocks: SimpleSFCBlock[] = [];
 
-      // Vertical split & Open all folds
-      await workspace.nvim.command('belowright vsplit || 1,$foldopen');
-
-      /** 2 */
-      await workspace.jumpTo(doc.textDocument.uri, Position.create(templateTagRange.start, templateTagRange.end));
-      // folding
-      await workspace.nvim.command(`${scriptTagRange.start + 1},${scriptTagRange.end + 1}fold`);
-      await workspace.nvim.command(`${styleTagRange.start + 1},${styleTagRange.end + 1}fold`);
-
-      // Split & Open all folds
-      await workspace.nvim.command('belowright split || 1,$foldopen');
-
-      /** 3 */
-      await workspace.jumpTo(doc.textDocument.uri, Position.create(styleTagRange.start, styleTagRange.end));
-      // folding
-      await workspace.nvim.command(`${scriptTagRange.start + 1},${scriptTagRange.end + 1}fold`);
-      await workspace.nvim.command(`${templateTagRange.start + 1},${templateTagRange.end + 1}fold`);
+    if (descriptor.scriptSetup || descriptor.script) {
+      blocksSet.push([descriptor.scriptSetup, descriptor.script].filter(shared.notEmpty));
+    }
+    if (descriptor.template) {
+      blocksSet.push([descriptor.template]);
+    }
+    if (descriptor.styles.length) {
+      blocksSet.push(descriptor.styles);
+    }
+    if (descriptor.customBlocks.length) {
+      blocksSet.push(descriptor.customBlocks);
     }
 
-    //
-    // MEMO: Two SFC
-    //
-    if (countSFCBlock == 2) {
-      if (scriptTagRange) {
-        /** 1 */
-        await workspace.jumpTo(doc.textDocument.uri, Position.create(scriptTagRange.start, scriptTagRange.end));
-
-        if (templateTagRange) {
-          // folding
-          await workspace.nvim.command(`${templateTagRange.start + 1},${templateTagRange.end + 1}fold`);
-        }
-        if (styleTagRange) {
-          // folding
-          await workspace.nvim.command(`${styleTagRange.start + 1},${styleTagRange.end + 1}fold`);
-        }
-
-        // Vertical split & Open all folds
-        await workspace.nvim.command('belowright vsplit || 1,$foldopen');
-
-        /** 2 */
-        if (templateTagRange) {
-          await workspace.jumpTo(doc.textDocument.uri, Position.create(templateTagRange.start, templateTagRange.end));
-          if (scriptTagRange) {
-            // folding
-            await workspace.nvim.command(`${scriptTagRange.start + 1},${scriptTagRange.end + 1}fold`);
-          }
-          if (styleTagRange) {
-            // folding
-            await workspace.nvim.command(`${styleTagRange.start + 1},${styleTagRange.end + 1}fold`);
-          }
-        } else if (styleTagRange) {
-          await workspace.jumpTo(doc.textDocument.uri, Position.create(styleTagRange.start, styleTagRange.end));
-          if (scriptTagRange) {
-            // folding
-            await workspace.nvim.command(`${scriptTagRange.start + 1},${scriptTagRange.end + 1}fold`);
-          }
-        }
-      } else if (templateTagRange) {
-        /** 1 */
-        await workspace.jumpTo(doc.textDocument.uri, Position.create(templateTagRange.start, templateTagRange.end));
-
-        if (styleTagRange) {
-          // folding
-          await workspace.nvim.command(`${styleTagRange.start + 1},${styleTagRange.end + 1}fold`);
-        }
-
-        // Vertical split & Open all folds
-        await workspace.nvim.command('belowright vsplit || 1,$foldopen');
-
-        /** 2 */
-        if (styleTagRange) {
-          await workspace.jumpTo(doc.textDocument.uri, Position.create(styleTagRange.start, styleTagRange.end));
-          // folding
-          await workspace.nvim.command(`${templateTagRange.start + 1},${templateTagRange.end + 1}fold`);
+    for (let i = 0; i < blocksSet.length; i++) {
+      for (const v of blocksSet[i]) {
+        switch (v.type) {
+          case 'script':
+            scriptBlocks.push({ type: v.type, start: v.loc.start.line, end: v.loc.end.line });
+            break;
+          case 'template':
+            templateBlocks.push({ type: v.type, start: v.loc.start.line, end: v.loc.end.line });
+            break;
+          case 'style':
+            styleBlocks.push({ type: v.type, start: v.loc.start.line, end: v.loc.end.line });
+            break;
+          default:
+            customBlocks.push({ type: v.type, start: v.loc.start.line, end: v.loc.end.line });
+            break;
         }
       }
     }
-  }
-}
 
-function getTagRange(doc: Document, tagType: 'script' | 'template' | 'style') {
-  let startRegex: RegExp;
-  let endRegex: RegExp;
+    let currentSplitCount = 0;
 
-  if (tagType === 'script') {
-    startRegex = /^<script/;
-    endRegex = /^<\/script>/;
-  } else if (tagType === 'template') {
-    startRegex = /^<template/;
-    endRegex = /^<\/template>/;
-  } else if (tagType === 'style') {
-    startRegex = /^<style/;
-    endRegex = /^<\/style>/;
-  } else {
-    return;
-  }
+    if (scriptBlocks.length >= 1) {
+      await workspace.jumpTo(doc.textDocument.uri, Position.create(scriptBlocks[0].start - 1, scriptBlocks[0].end - 1));
 
-  const start = getTagLine(doc, startRegex);
-  const end = getTagLine(doc, endRegex);
+      if (templateBlocks.length >= 1) await foldingSFCBlock(templateBlocks);
+      if (styleBlocks.length >= 1) await foldingSFCBlock(styleBlocks);
+      if (customBlocks.length >= 1) await foldingSFCBlock(customBlocks);
 
-  if (start !== undefined && end !== undefined) {
-    return {
-      start,
-      end,
-    };
-  }
-
-  return;
-}
-
-function getTagLine(doc: Document, regex: RegExp): number | undefined {
-  let matchLine: number | undefined;
-
-  const text = doc.textDocument.getText();
-  const textArr = text.split('\n');
-  textArr.some((line, index) => {
-    if (line.match(regex)) {
-      matchLine = index;
-      return true;
+      currentSplitCount++;
+      await doSplitWindow(blocksSet.length, currentSplitCount);
     }
-  });
 
-  return matchLine;
+    if (templateBlocks.length >= 1) {
+      await workspace.jumpTo(
+        doc.textDocument.uri,
+        Position.create(templateBlocks[0].start - 1, templateBlocks[0].end - 1)
+      );
+
+      if (scriptBlocks.length >= 1) await foldingSFCBlock(scriptBlocks);
+      if (styleBlocks.length >= 1) await foldingSFCBlock(styleBlocks);
+      if (customBlocks.length >= 1) await foldingSFCBlock(customBlocks);
+
+      currentSplitCount++;
+      await doSplitWindow(blocksSet.length, currentSplitCount);
+    }
+
+    if (styleBlocks.length >= 1) {
+      await workspace.jumpTo(doc.textDocument.uri, Position.create(styleBlocks[0].start - 1, styleBlocks[0].end - 1));
+
+      if (scriptBlocks.length >= 1) await foldingSFCBlock(scriptBlocks);
+      if (templateBlocks.length >= 1) await foldingSFCBlock(templateBlocks);
+      if (customBlocks.length >= 1) await foldingSFCBlock(customBlocks);
+
+      currentSplitCount++;
+      await doSplitWindow(blocksSet.length, currentSplitCount);
+    }
+
+    if (customBlocks.length >= 1) {
+      await workspace.jumpTo(doc.textDocument.uri, Position.create(customBlocks[0].start - 1, customBlocks[0].end - 1));
+
+      if (scriptBlocks.length >= 1) await foldingSFCBlock(scriptBlocks);
+      if (templateBlocks.length >= 1) await foldingSFCBlock(templateBlocks);
+      if (styleBlocks.length >= 1) await foldingSFCBlock(styleBlocks);
+
+      currentSplitCount++;
+      await doSplitWindow(blocksSet.length, currentSplitCount);
+    }
+  }
+}
+
+function useDocDescriptor() {
+  const splitDocText = ref('');
+  const splitDocDescriptor = computed(
+    () => parse(splitDocText.value, { sourceMap: false, ignoreEmpty: false }).descriptor
+  );
+
+  return getDescriptor;
+
+  function getDescriptor(text: string) {
+    splitDocText.value = text;
+    return splitDocDescriptor.value;
+  }
+}
+
+async function foldingSFCBlock(blockRanges: SimpleSFCBlock[]) {
+  for (const v of blockRanges) {
+    await workspace.nvim.command(`${v.start},${v.end}fold`);
+  }
+}
+
+async function doSplitWindow(maxSplitCount: number, currentSplitCount: number) {
+  if (maxSplitCount > currentSplitCount) {
+    if (currentSplitCount % 2 === 1) {
+      await workspace.nvim.command('belowright vsplit || 1,$foldopen');
+    }
+    if (currentSplitCount % 2 === 0) {
+      await workspace.nvim.command('belowright split || 1,$foldopen');
+    }
+  }
 }
