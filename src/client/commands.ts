a7a60234d232ac1046c372c09aba5f2c44b463a7
yaegassy <yaegassy@users.noreply.github.com>
2022-02-01 11:55:42 +0900

    Revert "feat: add typescript, javascript and related to jsx to activationEvents (#114)"
    This reverts commit ff5145822140bdaa025cd9dfaffeb74e25b7572f.

--- src/client/commands.ts
+++ src/client/commands.ts
@@ -7,11 +7,8 @@ export function doctorCommand(context: ExtensionContext) {
     const { document } = await workspace.getCurrentState();
     const filePath = Uri.parse(document.uri).fsPath;
 
-    const supportlanguages = ['vue', 'javascript', 'typescript', 'javascriptreact', 'typescriptreact'];
-    if (!supportlanguages.includes(document.languageId)) {
-      return window.showInformationMessage(
-        'volar.doctor has failed. The current languageId does not support the volar.doctor command.'
-      );
+    if (!filePath.endsWith('.vue')) {
+      return window.showInformationMessage('Failed to doctor. Make sure the current file is a .vue file.');
     }
 
     const clientJSON = path.join(context.extensionPath, 'package.json');
@@ -100,7 +97,7 @@ export function doctorCommand(context: ExtensionContext) {
 
 export function initializeTakeOverModeCommand() {
   return async () => {
-    const enableTakeOverMode = await window.showPrompt('Enable Take Over Mode?');
+    const enableTakeOverMode = await window.showPrompt('Enable TakeOverMode?');
     const config = workspace.getConfiguration('volar');
     const tsserverConfig = workspace.getConfiguration('tsserver');
 
